c3da12e6b455b486eb7ad2d5eece2798c5932a10
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-03-03 11:42:19 +0530

    initial commit

---
+++ src/components/SampleDisplay.js
@@ -0,0 +1,11 @@
+const SampleDisplay = (props) => {
+  return (
+    <section className="sample-display">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce purus
+      ligula, porta quis tempus porttitor, fermentum sed magna. Ut porta urna
+      nec enim laoreet aliquet.{" "}
+    </section>
+  );
+};
+
+export default SampleDisplay;
